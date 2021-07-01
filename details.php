1cca3fd2b3e486fd3159210c61c08eabb1f42b95
Aayush Bhatt <aayushbhatt20@gmail.com>
2021-07-01 23:18:08 +0530

    post form data to mysql db through xampp server

--- details.php
+++ details.php
@@ -10,3 +10,30 @@ if(!$conn)
     die("connection failed:". mysqli_connect_error());
 
 }
+
+
+if(isset($_POST['save']))
+{
+    $name = $_POST['name']; 
+    $number = $_POST['number'];
+    $email = $_POST['email'];
+    $subject = $_POST['subject'];
+
+    $sql_query = "Insert into contact(name,number,email,subject)
+    VALUES
+    ('$name','$number','$email','$subject')";
+      
+    if(mysqli_query($conn,$sql_query))
+     {
+        $message = "Your contact information is saved successfully.";
+        $type = "success";
+
+     }
+    else
+     {
+         echo "error:" .$sql."". mysqli_error($conn);
+            }
+            mysqli_close($conn);
+
+}
+?>
